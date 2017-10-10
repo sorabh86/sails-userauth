7cf577000fcc81d2eb3afbf784a02c236076f29e
Sorabh <ssorabh.ssharma@gmail.com>
2017-10-10 16:59:27 +0530

    added toaster

--- assets/js/public/login/LoginCtrl.js
+++ assets/js/public/login/LoginCtrl.js
@@ -1,7 +1,24 @@
 angular.module('LoginMod')
 
-.controller('LoginCtrl', ['$scope', '$http', function($scope, $http){
+.controller('LoginCtrl', ['$scope', '$http', 'toastr', function($scope, $http, toastr){
 	$scope.runLogin = function(){
-		console.log('login submitted');
+		$http.put('/login',{
+			email:$scope.email,
+			password:$scope.password
+		}).then(function(res){
+			window.location = '/';
+		}).catch(function(err){
+			if(err.status == 400 || 404) {
+				toastr.error('Invalid Credentials', 'Error', {
+					closeButton:true
+				});
+				return;
+			}
+
+			toastr.error('An error has occured, please try again later', 'Error', {
+				closeButton:true
+			});
+			return;
+		});
 	};
 }]);
